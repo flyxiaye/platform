f79870c832409a90e798d7ed305745e147bdf841
flyxiaye <962206819@qq.com>
2020-07-17 20:56:57 +0800

    debug2test3

--- sample/test/xmu_venc.h
+++ sample/test/xmu_venc.h
@@ -7,5 +7,5 @@ int venc_init(void);
 void venc_start(void);
 void venc_close(void);
 void venc_thread_sem_post(void);
-void enc_pair_set_source(struct video_input_frame *frame)
+void enc_pair_set_source(struct video_input_frame *frame);
 #endif
