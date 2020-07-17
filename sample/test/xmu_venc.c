c475c46f698c86699b03bdd39f8f611a300a52bd
flyxiaye <962206819@qq.com>
2020-07-17 21:30:00 +0800

    test3debug

--- sample/test/xmu_venc.c
+++ sample/test/xmu_venc.c
@@ -89,7 +89,9 @@ void *video_encode_from_vi_th(void *arg)
 
     while(1)
     {
+        ak_print_normal(MODULE_ID_VENC, "behind enc\n");
         ak_thread_sem_wait(&enc_sem);  //线程等待信号量
+        ak_print_normal(MODULE_ID_VENC, "after enc\n");
         int ret = ak_venc_encode_frame(venc_th->venc_handle, frame->vi_frame.data, frame->vi_frame.len, frame->mdinfo, stream);
         if (ret)
         {
